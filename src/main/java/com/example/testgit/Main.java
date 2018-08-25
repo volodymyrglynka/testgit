529cee3cda28d220913b61973bfb6b485d3d0af0
andriy.glynka <nalivajj09@gmail.com>
2018-08-25 15:42:18 +0300

    Test change

--- src/main/java/com/example/testgit/Main.java
+++ src/main/java/com/example/testgit/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hi, Andriy");
         System.out.println("Bye, Andriy!");
+        System.out.println("Hi, Volodymyr");
     }
 }
